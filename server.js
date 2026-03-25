4d4196c7b79cf8bb6f40aa90583e10dc1a0c4491
unknown <you@example.com>
2026-03-25 20:41:43 +0530

    fix cors

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const app = express();
 
 // ── MIDDLEWARE ──
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: function(origin, callback) {
+    callback(null, true);
+  },
   credentials: true
 }));
 app.use(express.json());
